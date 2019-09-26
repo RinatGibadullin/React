8ecb39215fbb8ac69250f6c6b30adae50aac9079
RinatGibadullin <Rinat990104@yandex.ru>
2019-09-26 20:15:26 +0300

    add items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './App.module.css';
-import  {Button, Input} from "antd";
+import {Button, Checkbox, Input} from "antd";
 
 function App() {
+    const todos = [1,2,3,4,5,6,7,8,9,10];
   return (
     <div className={styles.container}>
         <div className={styles.content}>
@@ -10,9 +11,21 @@ function App() {
                 <Input/>
                 <Button type="primary" className={styles.actionButton}> +Add </Button>
             </div>
+            {todos.map(() => <TodoItem/>)}
         </div>
     </div>
   );
+}
+
+function TodoItem() {
+    return (
+        <div className={styles.listContainer}>
+        <div className={styles.listItem}>
+            <Checkbox/>
+            <h2 className={styles.todoTitle}>dick</h2>
+        </div>
+    </div>
+    )
 }
 
 export default App;
